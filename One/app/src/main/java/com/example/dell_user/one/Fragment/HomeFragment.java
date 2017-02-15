0af2536029a51fccd6db5731423fa466da3445dc
Your Name <you@example.com>
2017-02-15 09:49:29 +0800

    Fragment

---
+++ One/app/src/main/java/com/example/dell_user/one/Fragment/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.example.dell_user.one.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dell_user.one.R;
+
+/**
+ * Created by wanjian on 2017/2/14.
+ */
+
+public class HomeFragment extends Fragment{
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.home,container,false);
+        return view;
+    }
+}
